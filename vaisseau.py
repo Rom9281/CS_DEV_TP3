2078223e8211dac64a1991b2ff05076d96b89983
lucasroth-github <lucas.roth@free.fr>
2020-12-15 10:32:23 +0100

    fichier vaisseau

---
+++ vaisseau.py
@@ -0,0 +1,13 @@
+import tkinter as tk 
+
+class vaisseau():
+    def __init__(self,x,y):
+        self.x1 = x1
+        self.y1 = y1 
+        self.x2 = x2
+        self.y2 = y2
+
+    def afficherVaisseau(self,main):
+        self.__vaisseau = main.create_rectangle(x1,y1,x2,y2)
+        
+        
