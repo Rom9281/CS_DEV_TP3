7a41994503241b65f398721c96118cd88e69a446
Romain GAUD <romaingaudfra@gmail.com>
2020-12-15 10:29:28 +0100

    ajout du deplacement progressif de l'alien vers le bas et d'un perdu quand il est tout en bas

--- alien.py
+++ alien.py
@@ -17,7 +17,8 @@ class alien():
 
         #Deplacement de l'alien
         self.__positif = True
-        self.__vit_deplacer = 5
+        self.__vit_deplacer_hor = 10 #Vitesse de deplacement horizontal
+        self.__vit_deplacer_ver = 100 #Vitesse de deplacement vertical
 
     def Getx1(self):
         return self.__x1
@@ -33,24 +34,33 @@ class alien():
 
     def GetColor(self):
         return self.__color_fill
+
     def ModifierCoord(self):
         if self.__positif:
-            if int(self.__canvas_len) > (self.__x2 + self.__vit_deplacer):
+            if int(self.__canvas_len) > (self.__x2 + self.__vit_deplacer_hor):
                 self.DeplacerDroit()
             else:
                 self.__positif = False
                 self.DeplacerGauche()
         else:
-            if 0 < (self.__x1 - self.__vit_deplacer):
+            if 0 < (self.__x1 - self.__vit_deplacer_hor):
                 self.DeplacerGauche()
             else:
                 self.__positif = True
                 self.DeplacerDroit()
+                if int(self.__canvas_hei) > (self.__y2 + self.__vit_deplacer_ver):
+                    self.DeplacerBas()
+                else:
+                    print("perdu")
             
     def DeplacerDroit(self):
-        self.__x1 += self.__vit_deplacer
-        self.__x2 += self.__vit_deplacer
+        self.__x1 += self.__vit_deplacer_hor
+        self.__x2 += self.__vit_deplacer_hor
     
     def DeplacerGauche(self):
-        self.__x1 += -self.__vit_deplacer
-        self.__x2 += -self.__vit_deplacer
+        self.__x1 += - self.__vit_deplacer_hor
+        self.__x2 += - self.__vit_deplacer_hor
+    
+    def DeplacerBas(self):
+        self.__y1 += self.__vit_deplacer_ver
+        self.__y2 += self.__vit_deplacer_ver
