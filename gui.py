ac7c37a3aa101abeaba21205608cbe7ef8112b03
lucasroth-github <lucas.roth@free.fr>
2020-12-15 08:50:53 +0100

    bouton quitter

--- gui.py
+++ gui.py
@@ -8,11 +8,17 @@ class gui():
 
     def AfficherFenetre(self):
         self.__main = tk.Tk()
+        
         #Ici le canvas
 
         #Ici la zone de score
 
         #ici le bouton quitter
+        bouton1 = tk.Button(self.__main , text='Quitter',command=self.__main.quit)
+        bouton1.pack()
+
         #Ici le bouton demarer
+
+
 
         self.__main.mainloop()
