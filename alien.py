d7b69c4c3a6c65e562bb5a2fdbdb78949a0ecad4
Romain GAUD <romaingaudfra@gmail.com>
2020-12-15 09:31:31 +0100

    Modification de l'alien: correction des parametres d'entree

--- alien.py
+++ alien.py
@@ -1,16 +1,26 @@
 import tkinter as tk
 
 class alien():
-    def __init__(self):
-        self.__x = 100
-        self.__y = 100
+    def __init__(self, canvas_len, canvas_hei):
+        self.__canvas_len = canvas_len  #Recupere la longueur du canvas
+        self.__canvas_hei = canvas_hei  #Recupere la hauteur du canvas
 
-        self.__length = 200
-        self.__height = 200
+        #Caracteristiques de l'alien
 
+        #Definition du carre a partir de coordonnes des points 1 (Coord x1 y1) et 2 (Coord x2 y2)
+        self.__x1 = 0
+        self.__y1 = 100
+        self.__x2 = 200
+        self.__y2 = 200
         self.__color_fill = "pink"
+
+        #Deplacement de l'alien
+        self.__sens = "+"
 
 
     
     def AfficherAlien(self,main):
-        main.create_rectangle(self.__x,self.__y,self.__length,self.__height, fill =self.__color_fill)
+        main.create_rectangle(self.__x1,self.__y1,self.__x2,self.__y2, fill =self.__color_fill)
+    
+    def ModifierCoord(self):
+        return False
